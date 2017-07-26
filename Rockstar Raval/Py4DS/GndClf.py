52a5ce9eb64b48221536803e771de1b854b66ee4
Ayush Shaw <ayushshaw@outlook.com>
2017-07-26 08:40:54 +0530

    Working on the Classifier Challenge

--- Rockstar Raval/Py4DS/GndClf.py
+++ Rockstar Raval/Py4DS/GndClf.py
@@ -4,15 +4,15 @@ from sklearn import tree
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
-
+# Gender
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-#classifier
+#Desision Tree classifier 
 clf = tree.DecisionTreeClassifier()
 
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([[143, 50, 33]])
 
-print(prediction)
+print("The Desision tree prediction is {}".format(prediction) )
